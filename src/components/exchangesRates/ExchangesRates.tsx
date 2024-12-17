bf208c2bda6844e6837e96758a6a82884d4b98a6
Angelina <angelinaivanova16@gmail.com>
2024-12-17 13:17:46 +0300

    fix: fix dates

--- src/components/exchangesRates/ExchangesRates.tsx
+++ src/components/exchangesRates/ExchangesRates.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import s from './exchangeRates.module.css';
 import type { ExchangeRatesData } from './exchangeRates.types';
 
@@ -6,28 +6,50 @@ const url = 'https://www.cbr-xml-daily.ru/daily_json.js'
 
 export const ExchangeRates = () => {
   const [data, setData] = useState<ExchangeRatesData[]>([])
-  const [currentDate, setCurrentDate] = useState()
-  const [previousDate, setPreviousDate] = useState()
+  const [currentDate, setCurrentDate] = useState('')
+  const [previousDate, setPreviousDate] = useState('')
 
   useEffect(() => {
     fetch(url)
       .then((result) => result.json())
-      .then((value) => {
-        setData(Object.values(value.Valute))
-        setCurrentDate(value.Date)
-        setPreviousDate(value.Timestamp)
+      .then((res) => {
+        setData(Object.values(res.Valute))
+        setCurrentDate(res.Date)
+        setPreviousDate(res.Timestamp)
       })
       .catch((err) => console.log(err))
 
   }, [])
+
+  const getCurrentDate = useMemo(
+    () => {
+      let curDate;
+    if (currentDate) {
+      const [year, month, day] = currentDate.split('-')
+      curDate = `${day.slice(0, 2)}.${month}.${year}.`
+    }
+    return curDate;
+    }, [currentDate]
+  );
+
+  const getPreviousDate = useMemo(
+    () => {
+      let prevDate;
+    if (previousDate) {
+      const [year, month, day] = previousDate.split('-')
+      prevDate = `${day.slice(0, 2)}.${month}.${year}.`
+    }
+    return prevDate;
+    }, [previousDate]
+  );
 
   return (
     <table>
       <thead className={s.tableHead}>
         <tr>
           <td>Валюта</td>
-          <td>{currentDate}</td>
-          <td>{previousDate}</td>
+          <td>{getCurrentDate}</td>
+          <td>{getPreviousDate}</td>
         </tr>
       </thead>
 
